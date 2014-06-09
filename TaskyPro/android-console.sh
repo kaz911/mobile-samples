a9dc71d9688d6c38c7c16dddc16388d2d3883ba2
Tom Opgenorth <tom@opgenorth.net>
2014-06-09 17:02:52 -0600

    Fixing up the Rake file and bash scripts for the updated Android project.

--- TaskyPro/android-console.sh
+++ TaskyPro/android-console.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export APK=Tasky.Android/bin/Release/com.xamarin.samples.taskydroid-Signed.apk
-#export ADB_DEVICE_ARG=192.168.56.101:5555 # Genymotion, Galaxy S3 w/Android 4.2.2
-#export ADB_DEVICE_ARG=0149B2EC03012005    # Galaxy Nexus
+export APK=Tasky.Droid/bin/Release/com.xamarin.samples.taskydroid-Signed.apk
+export ADB_DEVICE_ARG=192.168.56.101:5555 # Genymotion, Galaxy S3 w/Android 4.2.2
 export ADB_DEVICE_ARG=192.168.56.102:5555  # GenyMotion, Nexus 4 w/Android 4.3
 #calabash-android console $APK -p android features/login.feature
 calabash-android console $APK -p android
--- TaskyPro/android-test.sh
+++ TaskyPro/android-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export APK=Tasky.Android/bin/Release/com.xamarin.samples.taskydroid-Signed.apk
-export ADB_DEVICE_ARG=192.168.56.102:5555
+export APK=Tasky.Droid/bin/Release/com.xamarin.samples.taskydroid-Signed.apk
+export ADB_DEVICE_ARG=192.168.56.101:5555
 calabash-android run $APK -p android
